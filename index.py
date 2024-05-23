0762dabd75e180df6da719b5130611910b9fea7c
Pajan <d47708394@alumnos.itr.edu.ar>
2024-05-23 06:50:29 -0600

    login medio raro pero es algo, habria que ver el logout k esta medio tosco

--- index.py
+++ index.py
@@ -1,10 +1,40 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 
 app=Flask(__name__)
+app.secret_key = 'patri'
 
-@app.route("/")
+users = {
+    'pajan': 'god',
+    'pocho': 'pochin'
+}
+
+@app.route('/')
 def home():
-    return render_template("home.html")
+    if 'username' in session:
+        return render_template("home.html")
+    return "You are not logged in <br><a href='/login'>Login</a>"
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        if username in users and users[username] == password:
+            session['username'] = username
+            flash('Login successful!')
+            return redirect(url_for('home'))
+        else:
+            flash('Invalid credentials, please try again.')
+    
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    flash('You have been logged out.')
+    return render_template('logout.html')
+
 
 @app.route("/resultados")
 def resultados():
